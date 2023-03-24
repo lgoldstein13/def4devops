e94f9e8eb977905cc778472047d377d11a65d440
CHATGPT <lara.goldstein@microsoft.com>
2023-03-24 11:16:38 -0500

    GPTCommit

--- insecuredemo.py
+++ insecuredemo.py
@@ -1,4 +1,4 @@
-import hashlib
+
 
 xs=[1,2,3,4,5,6,7,8]
 try:
@@ -9,14 +9,15 @@ except: pass
 ys=[1, 2, None, None]
 for y in ys:
     try:
-        print(str(y+3)) #TypeErrors ahead
-    except: continue #not how to handle them
+        if y is not None:
+            print(str(y+3))
+    except: continue
 
 #some imports
 import telnetlib
 import ftplib
 
 s = b"I am a string"
-print("MD5: " +hashlib.md5(s).hexdigest())
-print("SHA1: " +hashlib.sha1(s).hexdigest())
+print("MD5: " +hashlib.sha256(s).hexdigest())
+print("SHA1: " +hashlib.sha256(s).hexdigest())
 print("SHA256: " +hashlib.sha256(s).hexdigest())
