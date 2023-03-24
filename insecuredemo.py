bd99eb2b25b18e5832a88d84a42ca28e34d00eb9
lgoldstein13 <114082360+lgoldstein13@users.noreply.github.com>
2023-03-24 10:46:19 -0500

    Update insecuredemo.py

--- insecuredemo.py
+++ insecuredemo.py
@@ -1,5 +1,3 @@
-
-
 import hashlib
 
 xs=[1,2,3,4,5,6,7,8]
@@ -11,13 +9,14 @@ except: pass
 ys=[1, 2, None, None]
 for y in ys:
     try:
-        if y is not None:
-            print(str(y+3))
-    except: continue
+        print(str(y+3)) #TypeErrors ahead
+    except: continue #not how to handle them
 
 #some imports
 import telnetlib
 import ftplib
 
 s = b"I am a string"
+print("MD5: " +hashlib.md5(s).hexdigest())
+print("SHA1: " +hashlib.sha1(s).hexdigest())
 print("SHA256: " +hashlib.sha256(s).hexdigest())
